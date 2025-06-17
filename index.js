c5ae1ff4bc2c9bf48ff55695f6d82dc655165d42
Your Name <your.email@example.com>
2025-06-17 15:20:11 +0300

    adds events

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+function addingEventListener (){
+const input = document.getElementById('button');
+input.addEventListener('click',function(){
+    alert('i was clicked')
+})
 }
+ 
+
+    
+
+
